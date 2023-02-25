a9f8113098898d994ba3222405752e32d2f4d4cf
guille garcia <ggarciacardiel@gmail.com>
2023-02-25 11:29:24 +0100

    adds theme swtich and counter

--- src/components/Switch.jsx
+++ src/components/Switch.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, {useState}  from 'react'
 
 const Switch = () => {
-  
+  const [counter, setCounter] = useState(0);
+  const [theme, setTheme] = useState('light');
+
+  const lightTheme = () => {
+    setCounter(counter + 1);
+    setTheme('light')
+  }
+
+  const darkTheme = () => {
+    setCounter(counter + 1);
+    setTheme('dark')
+  }
+
   return (
-    <div className="switch light">
+    <div className={`switch ${theme}`}>
       <h2>Theme picker</h2>
-      <h4>Times changed: </h4>
+      <h4>Times changed: {counter} </h4>
+      <button onClick={lightTheme}>Light Mode</button>
+      <button onClick={darkTheme}>Dark Mode</button>
     </div>
   )
 }
